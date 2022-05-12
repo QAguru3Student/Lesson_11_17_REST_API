c036b0479c66f96465ce7165a6044a1341323859
Babushkin Anton <napishi_antonu@mail.ru>
2022-05-12 18:46:25 +0300

    added allure

--- src/test/java/tests/DemoWebshopTests.java
+++ src/test/java/tests/DemoWebshopTests.java
@@ -3,11 +3,13 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static listeners.CustomAllureListener.withCustomTemplates;
 
 public class DemoWebshopTests {
    @Test
     void logInTest() {
                 given()
+                        .filter(withCustomTemplates())
                         .contentType("application/x-www-form-urlencoded")
                         .formParam("Email", "n.kozyrev@pik-remont.ru")
                         .formParam("Password", "12341234")
